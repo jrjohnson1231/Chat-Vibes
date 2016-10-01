82a3268dc6242603f3aae3b025527a0240f8c69e
John <jjohns48@nd.edu>
2016-10-01 16:54:30 -0400

    filetered data

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,5 @@
 (function (chrome) {
-    console.log('hello world');
     function makeRequest (data) {
-      document.querySelector("#message-input").value += "xxxxx";
       return $.ajax({
         url: '//ndhacks2016.herokuapp.com/tone', 
         type: 'POST', 
@@ -11,7 +9,7 @@
     }
 
     function handler(data) {
-      return Rx.Observable.fromPromise(makeRequest(data))
+      return Rx.Observable.fromPromise(makeRequest(data));
     }
 
     var textInput = document.querySelector('#message-input');
@@ -23,10 +21,23 @@
     .debounce(500)
     .distinctUntilChanged();
 
-    var output = throttledInput.flatMapLatest(handler);
+    var output = throttledInput.flatMapLatest(handler)
 
     output.subscribe(
     function (data) {
+      data = data.document_tone.tone_categories.map(function(category) {
+        return category.tones.map(function(tone) {
+          tone.category = category.category_name;
+          delete tone.tone_id;
+          return tone;
+        });
+      }).reduce(function(res, cur) {
+        Array.prototype.push.apply(res, cur);
+        return res;
+      }).filter(function(tone) {
+        return +tone.score > .6;
+      });
+      
       console.log(data);
     },
     function (e) {
