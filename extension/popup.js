81dfbf4d8462d893f64c45eb6eb659d65c05ddcb
rsimari <rsimari@nd.edu>
2016-10-01 15:08:30 -0400

    small changes

--- extension/popup.js
+++ extension/popup.js
@@ -1,41 +1,36 @@
 (function (chrome) {
-  var js = document.createElement('script');
-  js.type = 'text/javascript';
-  // js.src = chrome.extension.getURL('inject.js');
-  // document.getElementsByTagName('head')[0].appendChild(js);
-
+    console.log('hello world');
     function makeRequest (data) {
-      document.querySelector('#message-input') += 😀;
       return $.ajax({
         url: '//0.0.0.0:8080/tone', 
         type: 'POST', 
         contentType: 'application/json', 
         data: JSON.stringify({text: data}),
-      }).promise();
+      }).promise()
     }
 
     function handler(data) {
-      return Rx.Observable.fromPromise(makeRequest(data));
+      return Rx.Observable.fromPromise(makeRequest(data))
     }
 
-    var textInput = document.querySelector('#message-input');
-    console.log(textInput);
+    var textInput = document.querySelector('#textInput');
+    console.log(Rx.DOM.keyup, textInput)
     var throttledInput = Rx.DOM.keyup(textInput)
-      .pluck('target','value')
-      .filter( function (text) {
-        return text.length > 2;
-      })
-      .debounce(500)
-      .distinctUntilChanged();
+    .pluck('target','value')
+    .filter( function (text) {
+      return text.length > 2;
+    })
+    .debounce(500)
+    .distinctUntilChanged();
+
     var output = throttledInput.flatMapLatest(handler);
 
     output.subscribe(
     function (data) {
-      console.log("DATA: ");
       console.log(data);
     },
     function (e) {
         console.log(e);
-    });
+    })
 
 }(chrome));
